52f2293c0f251847e7f02bf6b8ed16679d79f05c
Kyle Shang <you@example.com>
2017-07-17 18:00:35 -0400

    initailize

---
+++ app/Http/Controllers/ChatsController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Message;
+use Illuminate\Support\Facades\Auth;
+
+class ChatsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * show chats
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('chat');
+    }
+
+    /**
+     * Fetch all messages
+     *
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function fetchMessages()
+    {
+        return Message::with('user')->get();
+    }
+
+    public function sendMessage(Request $request)
+    {
+        //get login user
+        $user = Auth::user();
+
+        //generate message model based on input message
+        $message = $user->messages()->create([
+            'message' => $request->input('message')
+        ]);
+
+        return ['status' => 'Message Sent!'];
+    }
+}
