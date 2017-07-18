b8348630db04bd7e46c95699b12e6ed412f2a5e6
Yachao Shang <yachaocs@gmail.com>
2017-07-17 21:20:59 -0400

    chat online

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,7 +16,49 @@ window.Vue = require('vue');
  */
 
 Vue.component('example', require('./components/Example.vue'));
+Vue.component('chat-messages', require('./components/ChatMessages.vue'));
+Vue.component('chat-form', require('./components/ChatForm.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+
+    data:{
+        messages:[]
+    },
+
+    created(){
+        //when the main component is created initialize messages
+        this.fetchMessages();
+
+        Echo.private('chat')
+            .listen('MessageSent', (e) => {
+                console.log('ok');
+                this.messages.push({
+                    message: e.message.message,
+                    user: e.user
+                });
+            });
+
+    },
+
+    methods:{
+
+        //get messages from server
+        fetchMessages(){
+            axios.get('/messages').then(response => {
+                this.messages = response.data;
+            });
+        },
+
+        //post messages to server
+        addMessage(message) {
+            this.messages.push(message);
+
+            axios.post('/messages', message).then(response => {
+                console.log(response.data);
+            });
+        }
+    }
+
+
 });
